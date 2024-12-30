4a56c6da77963a8c41db46eefb2b19f61c324d31
Maxim Kachinkin <m.kachinkin@dodobrands.io>
2024-12-30 22:54:15 +0300

    Add Out Of Stock state to finish

--- app/src/main/java/ru/otus/compose/examples/animations/productcard/finish/ProductCard.kt
+++ app/src/main/java/ru/otus/compose/examples/animations/productcard/finish/ProductCard.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -29,15 +30,22 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import ru.otus.compose.R
 import ru.otus.compose.examples.animations.productcard.finish.ProductCardState.ColorsState
 import ru.otus.compose.examples.animations.productcard.finish.ProductCardState.SizesState
+import ru.otus.compose.examples.animations.productcard.finish.ProductCardState.ImagesState.ImageState
 
 @Composable
 fun ProductCard(
@@ -58,7 +66,7 @@ fun ProductCard(
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
                 Images(
-                    imageRes = state.images.imagesRes[state.images.currentImage],
+                    image = state.images.imagesRes[state.images.currentImage],
                     modifier = Modifier,
                 )
                 ColorControls(
@@ -116,19 +124,48 @@ fun AboutProduct(
 
 @Composable
 fun Images(
-    imageRes: Int,
+    image: ImageState,
     modifier: Modifier = Modifier,
 ) {
-    Image(
-        painter = painterResource(imageRes),
-        contentDescription = null,
+    Box(
         modifier = modifier
-            .padding(horizontal = 20.dp)
-            .aspectRatio(1f)
             .fillMaxWidth()
-            .clip(shape = RoundedCornerShape(20.dp))
+    ) {
+        Image(
+            painter = painterResource(image.imageRes),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .aspectRatio(1f)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(20.dp))
+                .then(
+                    if (image.outOfStock) {
+                        Modifier.alpha(0.5f)
+                    } else {
+                        Modifier
+                    }
+                )
+        )
 
-    )
+        if (image.outOfStock) {
+            Text(
+                text = "Out of stock 😢",
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.error,
+                modifier = Modifier
+                    .padding(20.dp)
+                    .graphicsLayer {
+                        shadowElevation = 12f
+                        shape = RoundedCornerShape(20.dp)
+                        clip = true
+                    }
+                    .background(color = MaterialTheme.colorScheme.errorContainer)
+                    .padding(horizontal = 20.dp, vertical = 10.dp)
+                    .align(Alignment.TopCenter)
+            )
+        }
+    }
 }
 
 @Composable
@@ -161,9 +198,16 @@ fun ColorControls(
                         .clip(CircleShape)
                         .then(
                             if (index == state.currentColor) {
+                                if (colorState.outOfStock) {
+
+                                }
                                 Modifier.border(
                                     width = 2.dp,
-                                    color = MaterialTheme.colorScheme.primary,
+                                    color = if (colorState.outOfStock) {
+                                        MaterialTheme.colorScheme.error
+                                    } else {
+                                        MaterialTheme.colorScheme.primary
+                                    },
                                     shape = CircleShape,
                                 )
                             } else {
@@ -181,6 +225,16 @@ fun ColorControls(
                             .size(20.dp)
                     )
                 }
+            }
+
+            if (state.colors[state.currentColor].outOfStock) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_error),
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.error,
+                    modifier = Modifier
+                        .align(CenterVertically)
+                )
             }
         }
     }
@@ -229,8 +283,7 @@ fun SizesControls(
                             }
                         )
                         .padding(8.dp)
-                        .clickable { onSizeClicked(index) }
-                    ,
+                        .clickable { onSizeClicked(index) },
                     textAlign = TextAlign.Center
                 )
             }
