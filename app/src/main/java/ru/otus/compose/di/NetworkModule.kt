06ea5e8f683dce991601bd778baa5eff9635897a
Maxim Kachinkin <m.kachinkin@dodobrands.io>
2025-01-01 22:35:31 +0300

    Add Http Cache

--- app/src/main/java/ru/otus/compose/di/NetworkModule.kt
+++ app/src/main/java/ru/otus/compose/di/NetworkModule.kt
@@ -1,13 +1,16 @@
 package ru.otus.compose.di
 
+import android.content.Context
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
+import okhttp3.Cache
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -43,13 +46,26 @@ object NetworkModule {
     @Provides
     @Singleton
     fun provideOtherInterceptorOkHttpClient(
-        otherInterceptor: HeaderInterceptor
+        otherInterceptor: HeaderInterceptor,
+        @ApplicationContext
+        context: Context,
     ): OkHttpClient {
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .addInterceptor(AuthorizationInterceptor())
             .addInterceptor(TimeStampInterceptor())
             .addInterceptor(otherInterceptor)
+            .cache(createCache(context))
             .build()
     }
+
+    private fun createCache(context: Context): Cache {
+        return Cache(
+            directory = context.cacheDir.resolve(HTTP_CACHE_DIR),
+            maxSize = CACHE_SIZE,
+        )
+    }
+
+    private const val HTTP_CACHE_DIR = "http_cache"
+    private const val CACHE_SIZE = 20L * 1024L * 1024L // 20 MB
 }
