f86225900a1a5cf9cc423dd1fdcfc5888b746e40
Maxim Kachinkin <m.kachinkin@dodobrands.io>
2025-03-08 16:47:03 +0300

    Add Tulip example

--- app/src/main/java/ru/otus/compose/examples/canvas/IconExamples.kt
+++ app/src/main/java/ru/otus/compose/examples/canvas/IconExamples.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.withTransform
 import androidx.compose.ui.tooling.preview.Preview
 
 
@@ -162,5 +163,122 @@ fun GetWeatherApp() {
 fun GetWeatherAppPreview() {
     Surface {
         GetWeatherApp()
+    }
+}
+
+@Composable
+fun DrawTulip() {
+    val petal1 = Brush.verticalGradient(
+        listOf(Color(0xFFef4d67), Color(0xFFd92e50))
+    )
+
+    val petal2 = Brush.verticalGradient(
+        listOf(Color(0xffcd324b), Color(0xffbc203c))
+    )
+
+    val petal3 = Brush.verticalGradient(
+        listOf(Color(0xffae182d), Color(0xffae182d))
+    )
+
+    val stemColor = Color(0xFF4CAF50)
+    val leafColor = Brush.verticalGradient(
+        listOf(Color(0xFF66BB6A), Color(0xFF56AB5A))
+    )
+    val leafShadowColor = Color(0xFF2C8F30)
+
+    Canvas(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(16.dp)
+    ) {
+        val width = size.width
+        val height = size.height
+
+        val stemPath = Path().apply {
+            moveTo(width * 0.49f, height * 0.4f)
+            lineTo(width * 0.51f, height * 0.4f)
+            lineTo(width * 0.54f, height * 0.96f)
+            lineTo(width * 0.46f, height * 0.96f)
+            close()
+        }
+        drawPath(
+            path = stemPath,
+            color = stemColor
+        )
+
+        val leftLeafPath = Path().apply {
+            moveTo(width * 0.5f, height * 0.9f)
+            quadraticTo(
+                width * 0.1f, height * 0.4f,
+                width * 0.2f, height * 0.65f
+            )
+            quadraticTo(
+                width * 0.3f, height * 0.9f,
+                width * 0.5f, height * 0.95f
+            )
+            close()
+        }
+        drawPath(path = leftLeafPath, brush = leafColor)
+
+        val leftLeafShadow = Path().apply {
+            moveTo(width * 0.44f, height * 0.88f)
+            lineTo(width * 0.24f, height * 0.65f)
+            quadraticTo(
+                width * 0.3f, height * 0.8f,
+                width * 0.44f, height * 0.88f
+            )
+            close()
+        }
+        drawPath(path = leftLeafShadow, color = leafShadowColor)
+
+        withTransform(
+            transformBlock = { scale(-1f, 1f, Offset(width * 0.5f, 0f)) },
+            drawBlock = {
+                drawPath(path = leftLeafPath, brush = leafColor)
+                drawPath(path = leftLeafShadow, color = leafShadowColor)
+            }
+        )
+
+        val centerPetalPath = Path().apply {
+            moveTo(width * 0.38f, height * 0.3f)
+            cubicTo(
+                width * 0.50f, height * 0.1f,
+                width * 0.50f, height * 0.1f,
+                width * 0.60f, height * 0.3f
+            )
+            close()
+        }
+        drawPath(path = centerPetalPath, brush = petal3)
+
+        val leftPetalPath = Path().apply {
+            moveTo(width * 0.36f, height * 0.4f)
+            cubicTo(
+                width * 0.26f, height * 0.1f,
+                width * 0.50f, height * 0.1f,
+                width * 0.57f, height * 0.4f
+            )
+            cubicTo(
+                width * 0.57f, height * 0.55f,
+                width * 0.43f, height * 0.55f,
+                width * 0.36f, height * 0.4f
+            )
+            close()
+        }
+        drawPath(path = leftPetalPath, brush = petal2)
+
+        withTransform(
+            transformBlock = { scale(-1f, 1f, Offset(width * 0.5f, 0f)) },
+            drawBlock = {
+                drawPath(path = leftPetalPath, brush = petal1)
+            }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun DrawTulipPreview() {
+    Surface {
+        DrawTulip()
     }
 }
